271e22f389a0fb937c8d90cd59d1190d3b6e7853
Eric Fernandez <effernandezr@gmail.com>
2020-08-26 20:42:33 -0400

    Implement map based services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package com.spring5projects.sfgpetclinic.services.map;
+
+import com.spring5projects.sfgpetclinic.model.Owner;
+import com.spring5projects.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package com.spring5projects.sfgpetclinic.services.map;
+
+import com.spring5projects.sfgpetclinic.model.Vet;
+import com.spring5projects.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
