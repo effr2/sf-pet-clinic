68b54acd124f172d1226aed1c3c34c74f7c7ca9c
Eric Fernandez <effernandezr@gmail.com>
2020-08-27 14:37:31 -0400

    Added Spring Config for services. closes #22

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.spring5projects.sfgpetclinic.services.map;
 
 import com.spring5projects.sfgpetclinic.model.Pet;
 import com.spring5projects.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.spring5projects.sfgpetclinic.services.map;
 
 import com.spring5projects.sfgpetclinic.model.Vet;
 import com.spring5projects.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
