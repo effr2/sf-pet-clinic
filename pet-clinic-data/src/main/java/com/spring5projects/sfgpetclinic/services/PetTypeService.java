e2f64c424b8cddc4f484f03a62b41deede0d6f13
Eric Fernandez <effernandezr@gmail.com>
2020-08-28 13:19:06 -0400

    Added PetTypeServiceMap implementation. closes #29

---
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.spring5projects.sfgpetclinic.services;
+
+import com.spring5projects.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
