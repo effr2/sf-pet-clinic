87e274686cb4ddf221bf64e5a57fc5871bcb5f58
Eric Fernandez <effernandezr@gmail.com>
2020-08-29 12:46:25 -0400

    Updated Owner with JPA entity mappings. closes #36

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/model/PetType.java
@@ -1,8 +1,15 @@
 package com.spring5projects.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
