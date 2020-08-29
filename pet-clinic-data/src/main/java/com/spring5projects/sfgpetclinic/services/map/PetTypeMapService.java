bff1192450f3780c43535f49f97aa0dd4e54744f
Eric Fernandez <effernandezr@gmail.com>
2020-08-29 16:50:22 -0400

    Updated for JPA services, tested active profiles. Closes #59

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/PetTypeMapService.java
@@ -2,11 +2,13 @@ package com.spring5projects.sfgpetclinic.services.map;
 
 import com.spring5projects.sfgpetclinic.model.PetType;
 import com.spring5projects.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
