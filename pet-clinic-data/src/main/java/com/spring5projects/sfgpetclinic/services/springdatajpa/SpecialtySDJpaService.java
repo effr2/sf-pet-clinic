2f3abd2b32f792f036d232f0e2cbe33e9cb66902
Eric Fernandez <effernandezr@gmail.com>
2020-08-29 16:31:31 -0400

    Implement Visit Spring Data JPA Service. Closes #47

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -3,10 +3,14 @@ package com.spring5projects.sfgpetclinic.services.springdatajpa;
 import com.spring5projects.sfgpetclinic.model.Specialty;
 import com.spring5projects.sfgpetclinic.repositories.SpecialtyRepository;
 import com.spring5projects.sfgpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class SpecialtySDJpaService implements SpecialtyService {
 
     private final SpecialtyRepository specialtyRepository;
