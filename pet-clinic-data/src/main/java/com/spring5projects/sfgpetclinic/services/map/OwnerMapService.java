444b7f4858c40ad5a89070afe720f557a7b5d5fc
Eric Fernandez <effernandezr@gmail.com>
2020-08-30 16:23:04 -0400

    Added tests to OwnerController. Closes #52

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.spring5projects.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -76,4 +77,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .findFirst()
                 .orElse(null);
     }
+
+//    @Override
+//    public List<Owner> findAllByLastNameLike(String lastName) {
+//
+//        //todo - impl
+//        return null;
+//    }
 }
