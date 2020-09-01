ec13dd5024e3717d86cfca783ea722a05f3cd448
Eric Fernandez <effernandezr@gmail.com>
2020-08-31 23:01:58 -0400

    Implement find owner functionality. Closes #54

--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.spring5projects.sfgpetclinic.repositories;
 import com.spring5projects.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/spring5projects/sfgpetclinic/services/OwnerService.java
@@ -2,9 +2,11 @@ package com.spring5projects.sfgpetclinic.services;
 
 import com.spring5projects.sfgpetclinic.model.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
